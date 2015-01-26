47443903c2369c12b445e6f87cdcebd8fd00a09f
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-01-26 12:10:10 -0500

    commented code for chapter 2 exercise

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,8 @@
-//Fizzbuzz code
+//Ryan Postma
+//Chapter 2 Fizzbuzz
+//CMP 237
+//1/26/15
+//sifts through numbers and prints out fizz, buzz, or fizzbuzz
 
 for (var i = 0; i <= 100; i++) {
   if (i % 3 == 0 && i % 5 == 0)
